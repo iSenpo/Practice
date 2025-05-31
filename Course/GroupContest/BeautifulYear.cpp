052ebc420b8d62e903932a7c422e7e94bf5aae4a
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-05-31 22:18:28 +0330

    Course

---
+++ Course/GroupContest/BeautifulYear.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int y;
+    cin >> y;
+    
+    
+    while(true){
+        y++;
+        int temp = y;
+        bool used[10] = {false};
+        bool uniqe = true;
+        
+        while(temp > 0){
+            int d = temp % 10;
+            if(used[d]){
+                uniqe = false;
+                break;
+            }
+            used[d] = true;
+            temp /= 10;
+        }
+        if(uniqe){
+            cout << y;
+            break;
+        }
+    }
+    
+return 0;
+}
