d917f3dbda4681fe23ef3ad282b2b98331537552
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-08-03 11:49:56 +0330

    dp

--- Course/DP/B_Contiguous_Repainting.cpp
+++ Course/DP/B_Contiguous_Repainting.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define ll long long
 int MOD = 1e9 + 7;
 const int N  = 1e5 + 500;
-int arr[N];
+int a[N];
 
 void solve()
 {
@@ -12,22 +12,27 @@ void solve()
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    /*---------------------------------*/
+    /*-------------------------------------*/
     int n , k;
     cin >> n >> k;
-    vector<ll> dp(n + 1 , 0);
-    for(int i = 0 ; i < n ; i++)
-        cin >> arr[i];
-    for(int i = 0; i < n ; i++)
-        if(arr[i] > 0)
-            dp[i] += arr[i];
-        
-    ll Max = 0;
-    for(int i = 0; i <= n-k ; i++){
-        ll left = i;
-        ll right = i + k - 1;
-        Max = max(Max , (dp[left] - dp[0])+(dp[right] - dp[left])+(dp[n-1] - dp[right]));
+    vector<ll> postive(n + 1 , 0);
+    vector<ll> ps(n + 1 , 0);
+
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i] ;
+        postive[i + 1] = postive[i] + max(0 , a[i]);
+        ps[i + 1] = ps[i] + a[i];
     }
-    cout << Max ;
+    ll ans = 0;
+    for(int i = 0 ; i + k <= n ; i++){
+        ll left = postive[i];
+        ll right = postive[n] - postive[i + k];
+        ll mid = ps[i + k] - ps[i];
+        if(mid < 0){
+            mid = 0;
+        }
+        ans = max(ans , mid + right + left);
+    }
+    cout << ans;
     return 0;
 }
