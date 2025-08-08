e52c1d08a87cae5e4b328d356a20bb22dc8fe42d
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-08-08 15:02:04 +0330

    DP

--- Course/DP/C_Vacation.cpp
+++ Course/DP/C_Vacation.cpp
@@ -3,14 +3,7 @@ using namespace std;
 #define ll long long
 #define ln '\n'
 #define fo(i, n) for(int i = 1 ; i <= n ; i++)
-int MOD = 1e9 + 7;
-const int N  = 1e5 + 500;
-int arr[N];
 
-void solve()
-{
-
-}
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
--- Course/DP/E_Knapsack_2.cpp
+++ Course/DP/E_Knapsack_2.cpp
@@ -1,16 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ln '\n'
-#define fo(i, n) for(int i = 0 ; i < n ; i++)
-int MOD = 1e9 + 7;
-const int N  = 1e9;
-int arr[N];
-
-void solve()
-{
-
-}
+const int N = 1e5;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -21,12 +12,19 @@ int main(){
     for(int i = 0 ; i < n ; i++){
         cin >> v[i] >> w[i] ;
     }
-    int dp[n];
-    memset(dp , N , n * sizeof(int));
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < x ; j++){
-            
+    vector<vector<int>> dp(n+1 , vector<int>(n +1 , N));
+    for(int i = 0 ; i <= n ; i++){
+        dp[0][i] = 0;
+        dp[i][0] = 0;
+    }
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = 1 ; j <= n ; j++){
+            dp[i][j] = dp[i-1][j];
+            if(j - v[i] >= 0){
+                dp[i][j] = min(dp[i-1][j] , dp[i-1][j-v[i]] + w[i]);
+            }
         }
     }
+    cout << dp[n][n];
     return 0;
 }
