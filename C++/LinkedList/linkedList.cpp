fd6a1199216e35705d2db0de9c109fe81a218673
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-09-09 13:26:42 +0330

    LinkedList

--- C++/LinkedList/linkedList.cpp
+++ C++/LinkedList/linkedList.cpp
@@ -8,12 +8,20 @@ class Node{
         Node* prev;
         Node(int d ,Node* n = NULL, Node* p = NULL):data(d) ,next(n) ,prev(p){}
 };
+//Recursive print
 void Print_List(Node* list){
     if(list == NULL){
         return;
     }
     cout << list->data << ' ';
     Print_List(list->next);
+}
+//Iterative print
+void Print_list2(Node* list){
+    for(list ; list ; list = list->next){
+        cout << list->data << ' ';
+    }
+    cout << '\n';
 }
 void append(Node* list , int x){
     Node* last = list;
@@ -24,12 +32,13 @@ void append(Node* list , int x){
     last->next = new_node;
 }
 int main(){
-    Node a(5);
+    Node* a = NULL;
     for(int i = 0 ; i < 5 ; i++){
         int x;
         cin >> x;
-        append(&a , x);
+        append(a , x);
     }
-    Print_List(&a);
+    Print_List(a);
+    Print_list2(a);
     return 0;
 }
