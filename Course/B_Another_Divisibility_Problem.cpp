091ab19e3aa46421f9161e39ae2eecafa5ebce71
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-09-15 16:32:31 +0330

    B_Division Rule

---
+++ Course/B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; i++)
+#define all(x) (x).begin(),(x).end()
+int MOD = 1e9 + 7;
+const int N  = 1e5 + 500;
+int arr[N];
+
+inline int in(){
+    int x;
+    cin >> x;
+    return x;
+}
+void solve()
+{
+    int x = in();
+    cout << 2 * x << ln;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*------------------------------------*/
+    int t = in();
+    while(t--){
+        solve();
+    }
+    return 0;
+}
