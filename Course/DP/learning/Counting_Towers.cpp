14056715e8d74f6f43fb55dad8f7b1dd7d18b995
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-09-19 15:41:12 +0330

    CSES

--- Course/DP/learning/Counting_Towers.cpp
+++ Course/DP/learning/Counting_Towers.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll unsigned long long
+#define ll long long
 #define ln '\n'
 #define fo(i, n) for(int i = 0 ; i < n ; i++)
 #define all(x) (x).begin(),(x).end()
 ll MOD = 1e9 + 7;
 const int N = 1e6;
-vector<vector<ll>> dp;
+vector<vector<ll>> dp(N + 1 , vector<ll>(6 , 0));
 inline int in(){
     int x;
     cin >> x;
     return x;
 }
+//Recursive Solution:
 ll solve(int n , int i)
 {
     //base :
@@ -19,45 +20,57 @@ ll solve(int n , int i)
         return 1;
     }
     //transition :
-    if(dp[n][i] != -1){
+    if(dp[n][i] != 0){
         return dp[n][i];
     }
     ll ans = 0;
-    if(i == 0){
-        ans += solve(n-1 , 0)%MOD;
-        ans += solve(n-1 , 2)%MOD;
-        ans += solve(n-1 , 1)%MOD;   
+    if(i == 0 || i == 2){
+        ans = (ans + solve(n-1 , 0))%MOD;
+        ans = (ans + solve(n-1 , 1))%MOD;   
+        ans = (ans + solve(n-1 , 2))%MOD;
     }
-    if(i == 1){
-        ans += solve(n-1 , 1)%MOD;   
-        ans += solve(n-1 , 2)%MOD;   
-        ans += solve(n-1 , 3)%MOD;   
-        ans += solve(n-1 , 4)%MOD;   
-        ans += solve(n-1 , 5)%MOD;   
+    if(i == 1 || i == 3 || i == 4 || i == 5){
+        ans = (ans + solve(n-1 , 1))%MOD;    
+        ans = (ans + solve(n-1 , 2))%MOD;
+        ans = (ans + solve(n-1 , 3))%MOD;   
+        ans = (ans + solve(n-1 , 4))%MOD;   
+        ans = (ans + solve(n-1 , 5))%MOD;   
     }
-    if(i == 2){
-        ans += solve(n-1 , 0)%MOD;   
-        ans += solve(n-1 , 1)%MOD;   
-        ans += solve(n-1 , 2)%MOD;      
+    return dp[n][i] = ans;
+}
+//Iterative Solution:
+ll solve2(){
+    //base:
+    for(int i = 0 ; i < 6 ; i++){
+        dp[1][i] = 1;
     }
-    if(i == 3 || i == 4 || i == 5){
-        ans += solve(n-1 , 1)%MOD;
-        ans += solve(n-1 , 2)%MOD;
-        ans += solve(n-1 , 3)%MOD;
-        ans += solve(n-1 , 4)%MOD;
-        ans += solve(n-1 , 5)%MOD;   
+    //transition:
+    for(int i = 2 ; i <= N ; i++){
+        for(int j = 0 ; j < 6 ; j++){
+            if(j == 0 || j == 2){
+                dp[i][j] = (dp[i][j] + dp[i-1][0] )%MOD;
+                dp[i][j] = (dp[i][j] + dp[i-1][1] )%MOD;
+                dp[i][j] = (dp[i][j] + dp[i-1][2] )%MOD;
+            }
+            if(j == 1 || j == 3 || j == 4 || j == 5){
+                dp[i][j] = (dp[i][j] + dp[i-1][1] )%MOD;
+                dp[i][j] = (dp[i][j] + dp[i-1][2] )%MOD;
+                dp[i][j] = (dp[i][j] + dp[i-1][3] )%MOD;
+                dp[i][j] = (dp[i][j] + dp[i-1][4] )%MOD;
+                dp[i][j] = (dp[i][j] + dp[i-1][5] )%MOD;
+            }
+        }
     }
-    return dp[n][i] = ans%MOD;
 }
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     /*------------------------------------*/
     int t = in();
+    solve2();
     while(t--){
         int n = in();
-        dp.resize(n + 5 , vector<ll>(10 , -1));
-        ll ans = (solve(n , 2)%MOD + solve(n , 1)%MOD)%MOD;
+        ll ans = (dp[n][1]+ dp[n][2])%MOD;
         cout << ans << ln;
     }
     return 0;
