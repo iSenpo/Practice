f7135ce87f0a88e5092687ea0ff78f5c1e3dba64
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-10-02 15:10:18 +0330

    A

---
+++ Course/Contest/486_Div3/A_Diverse_Team.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+
+const int N  = 1e5 + 500;
+int arr[N];
+typedef pair<int , bool> pii;
+inline int in(){
+    int x;
+    cin >> x;
+    return x;
+}
+void solve()
+{
+    int n = in();
+    int k = in();
+    int a[n];
+    map<int , bool> ans;
+    vector<int> print;
+    int cnt  = 0;
+    fo(i , n){
+        cin >> a[i];
+        if(!ans[a[i]]){
+            print.pb(i + 1);
+            ans[a[i]] = true;
+            cnt++;
+        }
+    }
+    if(cnt >= k){
+        cout << "YES\n";
+        for(int i : print){
+            cout << i << ' ';
+        }
+    }
+    else{
+        cout << "NO\n";
+    }
+
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*------------------------------------*/
+    int t = 1;
+    //t = in();
+    while(t--){
+        solve();
+    }
+    return 0;
+}
