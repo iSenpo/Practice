c4e8e6ec5178362e58a1cb23475859dbf196b4ae
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-10-03 13:42:38 +0330

    A

---
+++ Course/Contest/A_Mishka_and_Contest.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+#define mt(x , y , z) make_tuple(x , y , z)
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+
+const int N  = 1e5 + 500;
+int arr[N];
+
+inline int in(){
+    int x;
+    cin >> x;
+    return x;
+}
+void solve()
+{
+    int n = in();
+    int k = in();
+    int a[n];
+    fo(i , n) cin >> a[i];
+    int ptr = n - 1;
+    int ptl = 0;
+    int ans = 0;
+    while(ptl <= ptr){
+        if(a[ptr] <= k){
+            ++ans;
+            --ptr;
+        }
+        else if(a[ptl] <= k){
+            ++ans;
+            ++ptl;
+        }
+        else{
+            break;
+        }
+    }
+    cout << ans << ln;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*------------------------------------*/
+    int t = 1;
+    //t = in();
+    while(t--){
+        solve();
+    }
+    return 0;
+}
