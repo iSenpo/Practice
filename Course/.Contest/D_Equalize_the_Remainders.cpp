04782285cab493cca0f12f62df100dbe5df7dc03
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-10-03 16:19:48 +0330

    update

---
+++ Course/.Contest/D_Equalize_the_Remainders.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+#define mt(x , y , z) make_tuple(x , y , z)
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+
+const int N  = 1e5 + 500;
+int arr[N];
+
+inline int in(){
+    int x;
+    cin >> x;
+    return x;
+}
+void solve()
+{
+    int n = in();
+    int k = in();
+    
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*------------------------------------*/
+    int t = 1;
+    //t = in();
+    while(t--){
+        solve();
+    }
+    return 0;
+}
