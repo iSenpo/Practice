fff8a34eaa59c28e438dfe7b0c64931cd0b606f4
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-10-09 08:47:28 +0330

    E

--- Course/.Contest/490_Div3/E_Reachability_from_the_Capital.cpp
+++ Course/.Contest/490_Div3/E_Reachability_from_the_Capital.cpp
@@ -14,78 +14,53 @@ const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
 int MOD = 1e9 + 7;
 
 const int N  = 5e3 + 10;
-vector<int> In[N];
-vector<int> Out[N];
 bool mark[N];
-
+vector<int> adj[N];
+stack<int> stk;
 
 void dfs(int v){
-    mark[v] = true;
-    for(int i = 0 ; i < Out[v].size() ; ++i){
-        int u = Out[v][i];
-        if(!mark[u]){
-            mark[u] = true;
-            dfs(u);
-        }
+    if(!mark[v]){
+        mark[v] = true;
+        for(int i : adj[v])
+            dfs(i);
     }
 }
-bool mark2[N];
-vector<int> w(N , 0);
 
-void dfs2(int v,int par){
-    if(mark[v]){
-        return;
+void dfs2(int v){
+    if(!mark[v]){
+        mark[v] = true;
+        stk.push(v);
+        for(int i : adj[v])
+            dfs(i);
     }
-    mark2[v] = true;
-    for(int i = 0 ; i < Out[v].size() ; ++i){
-        if(!mark2[Out[v][i]]){
-            dfs2(Out[v][i] , par);
-        }
-    }
-    w[par]++;
 }
 void solve()
 {
     int n , m  , s;
     cin >> n >> m >> s;
+    s--;
     fo(i , m){
         int v, u;
         cin >> v >> u;
-        Out[v].pb(u);
-        In[u].pb(v);
+        v--;
+        u--;
+        adj[v].pb(u);
     }
     dfs(s);
-    int ans = 0;
-    vector<int> best;
-    for(int i = 1 ; i <= n ; ++i){
-        if(In[i].size() == 0){
-            best.pb(i);
+    for(int i = 0 ; i < n ; ++i){
+        if(!mark[i]){
+            dfs2(i);
         }
     }
-    int t = best.size();
-    for(int i = 0 ; i < t ; ++i){
-       dfs2(best[i] , best[i]);
-       memset(mark2 , false , sizeof mark2);
-    }
-    for(int i = 0 ; i < t - 1 ; ++i){
-        for(int j = 0 ; j < t - i - 1 ; ++j){
-            if(w[best[j + 1]] < w[best[j + 2]]){
-                swap(best[j + 1] , best[j + 2]);
-                swap(w[best[j + 1]] , w[best[j + 2]]);
-            }
-        }
-    }
-    for(int i = 0 ; i < t ; ++i){
-        int v = best[i];
+    ll ans = 0;
+    memset(mark , false , sizeof mark);
+    while(!stk.empty()){
+        int v = stk.top();
+        stk.pop();
         if(!mark[v]){
-            In[v].pb(s);
-            Out[s].pb(v);
             dfs(v);
             ans++;
         }
-    }
-    if(In[s].size() == 0 && Out[s].size() == 0){
-        ans++;
     }
     cout << ans << ln;
 }
