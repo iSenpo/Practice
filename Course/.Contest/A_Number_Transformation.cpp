472a9c3a01287a8860a2f179181c4eb58a37f122
Shayan <shayan.erfanian.sh@gmail.com>
2025-10-11 17:58:36 +0330

    ok

---
+++ Course/.Contest/A_Number_Transformation.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int , int>
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+#define mt(x , y , z) make_tuple(x , y , z)
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+
+const int N  = 2e5 + 10;
+//always check input!!!
+
+void solve()
+{
+    int x , y;
+    cin >> x >> y;
+    if(x > y){
+        cout << "0 0\n";
+        return;
+    }
+    if(y%x){
+        cout << "0 0\n";
+        return;
+    }
+    cout << 1 << ' ' << y/x << ln;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*------------------------------------*/
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+//check long long memory limit
+//check int overflow
