5a28b84c5e43742906b73b50254d6943cc89b5b6
Shayan <shayan.erfanian.sh@gmail.com>
2025-11-02 13:58:26 +0330

    solved

--- Course/.Problemset/C_Non_Descending_Arrays.cpp
+++ Course/.Problemset/C_Non_Descending_Arrays.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
 #define pii pair<int , int>
 #define ln '\n'
@@ -9,64 +10,63 @@ using namespace std;
 #define pb(x) push_back(x)
 #define mt(x , y , z) make_tuple(x , y , z)
 
-// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
-
-const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
-const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
-
-const int N  = 2e5 + 10;
-//always check input!!!
-
-int MOD = 998244353;
+const int N = 2e5 + 10;
+const int MOD = 998244353;
 int n;
-vector<int> a(101 , 0) , b(101 , 0);
+vector<int> a(101 , 0), b(101 , 0);
+vector<vector<int>> memo(101, vector<int>(2, -1));
 
 ll dp(int i , bool swaped){
     if(i == n){
         if(!swaped){
             if(a[i] >= a[i - 1] && b[i] >= b[i - 1]){
                 return 1;
-            }
-            else{
+            } else {
                 return 0;
             }
         }
         else{
-           if(a[i] >= b[i - 1] && b[i] >= a[i - 1]){
+            if(a[i] >= b[i - 1] && b[i] >= a[i - 1]){
                 return 1;
-            }
-            else{
+            } else {
                 return 0;
             } 
         }
     }
+    if(memo[i][swaped] != -1){
+        return memo[i][swaped];
+    }
     if(swaped){
-        if(a[i] < b[i -1] || b[i] < a[i - 1]){
-            return 0;
+        if(a[i] < b[i - 1] || b[i] < a[i - 1]){
+            return memo[i][swaped] = 0;
         }
     }
     else{
         if(a[i] < a[i - 1] || b[i] < b[i - 1]){
-            return 0;
+            return memo[i][swaped] = 0;
         }
     }
-    return (dp(i + 1 , true) + dp(i + 1 , false)) % MOD;
+    return memo[i][swaped] = (dp(i + 1 , true) + dp(i + 1 , false)) % MOD;
 }
+
 void solve()
 {
     cin >> n;
-    for(int i = 1; i <= n ; i++){
+    for(int i = 1; i <= n; i++){
         cin >> a[i];
     }
-    for(int i = 1; i <= n ; i++){
+    for(int i = 1; i <= n; i++){
         cin >> b[i];
+    }
+    for (int i = 0; i <= n; ++i) {
+        fill(memo[i].begin(), memo[i].end(), -1);
     }
     cout << (dp(1 , true) + dp(1 , false)) % MOD << ln;
 }
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    /*------------------------------------*/
     int t = 1;
 
     cin >> t;
@@ -75,5 +75,3 @@ int main(){
     }
     return 0;
 }
-//check long long memory limit
-//check int overflow
