0c512a73009d4659fd6f23e8ac8f2c77e61c5f35
Shayan <shayan.erfanian.sh@gmail.com>
2025-12-22 23:13:55 +0330

    up

---
+++ Course/.Contest/Div3/544_Div3/F_1_Spanning_Tree_with_Maximum_Degree.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int , int>
+#define pll pair<ll , ll>
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+#define	set_dec(x) cout << fixed << setprecision(x);
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+const int N  = 2e5 + 10;
+const int INF = 2e9 + 100;
+const int MIN = -2e9 - 100;
+//always check input!!!
+vector<int> adj[N];
+int deg[N];
+bool mark[N];
+
+void solve()
+{
+    int n , m;
+    cin >> n >> m;
+    vector<pii> ans;
+    fo(i , m){
+        int v , u;
+        cin >> v >> u;
+        v--;
+        u--;
+        adj[v].pb(u);
+        adj[u].pb(v);
+        deg[v]++;
+        deg[u]++;
+    }
+    int pos = 0;
+    fo(i , n){
+        if(deg[pos] < deg[i]){
+            pos = i;
+        }
+    }
+    queue<int> bfs;
+    bfs.push(pos);
+    mark[pos] = true;
+    while(!bfs.empty()){
+        int v = bfs.front();
+        bfs.pop();
+        mark[v] = true;
+        for(int i : adj[v]){
+            if(!mark[i]){
+                bfs.push(i);
+                mark[i] = true;
+                ans.push_back({v , i});
+            }
+        }
+    }
+    for(pii i : ans){
+        cout << i.first + 1 << ' ' << i.second + 1 << ln;
+    }
+}
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
