c89525ed3430491c5120a20b94c5b4d8be25f032
Shayan <shayan.erfanian.sh@gmail.com>
2026-01-04 12:16:23 +0330

    up

---
+++ Course/.Contest/Div3/719_Div3/B_Ordinary_Numbers.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int , int>
+#define pll pair<ll , ll>
+#define ln '\n'
+#define fo(i, n) for(int i = 0 ; i < n ; ++i)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define sz(x) (int)(x).size()
+#define pb(x) push_back(x)
+#define	set_dec(x) cout << fixed << setprecision(x);
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+int MOD = 1e9 + 7;
+const int N  = 2e5 + 10;
+const int INF = 2e9 + 100;
+const int MIN = -2e9 - 100;
+//always check input!!!
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll d = n , cnt = 0 , last = 0;
+    while(d > 0){
+        cnt++;
+        last = d % 10;
+        d /= 10;
+    }
+    ll ans = (cnt - 1) * 9;
+    ll check = 0;
+    while(cnt--){
+        check += last * (ll)pow(10 , cnt);
+    }
+    ans += (last - 1);
+    if(n >= check){ 
+        ans++;
+    }
+    cout << ans << ln;
+}
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
